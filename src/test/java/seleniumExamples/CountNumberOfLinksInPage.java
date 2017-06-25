5ad7576b27349eb6ecfd0a4579ff5878ef20dc6a
Singh, Bhanu Pratap <BhanuPratap_Singh@intuit.com>
2017-06-25 22:04:09 +0530

    add

--- src/test/java/seleniumExamples/CountNumberOfLinksInPage.java
+++ src/test/java/seleniumExamples/CountNumberOfLinksInPage.java
@@ -1,35 +1,29 @@
 package seleniumExamples;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class SelectDropDownInSelenium {
 
+public class CountNumberOfLinksInPage {
+
+	
 	WebDriver driver;
 
 	@Test
-	public void selectDropDownInSelenium() throws InterruptedException {
+	public void countNumberOfLinksInPage() throws InterruptedException {
 		// for Mac OS
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver");
 		// For Window OS
 		// System.setProperty("webdriver.gecko.driver",System.getProperty("user.dir") + "/drivers/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
-		
-		Select select = new Select(driver.findElement(By.xpath(".//*[@id='day']")));
-		
-		select.selectByIndex(3);
-		
-		select = new Select(driver.findElement(By.xpath(".//*[@id='month']")));
-		
-		select.selectByVisibleText("Jan");
-		
-		select = new Select(driver.findElement(By.xpath(".//*[@id='year']")));
-		
-		select.selectByValue("2014");
-	
+	    List<WebElement> links = driver.findElements(By.xpath("//a"));
+	    Assert.assertEquals(links.size(), "53");
 	}
 }
