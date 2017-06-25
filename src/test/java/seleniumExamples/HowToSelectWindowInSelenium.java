f84dd7e4f4409bd7d2b1d928fe9cc52acd66a900
Singh, Bhanu Pratap <BhanuPratap_Singh@intuit.com>
2017-06-25 22:16:39 +0530

    add

---
+++ src/test/java/seleniumExamples/HowToSelectWindowInSelenium.java
@@ -0,0 +1,42 @@
+package seleniumExamples;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HowToSelectWindowInSelenium {
+	
+
+	WebDriver driver;
+
+	@Test
+	public void howToSelectWindowInSelenium() throws InterruptedException {
+		// for Mac OS
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver");
+		// For Window OS
+		// System.setProperty("webdriver.gecko.driver",System.getProperty("user.dir") + "/drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+	    driver.get("http://www.hdfcbank.com/htdocs/common/onlineservices/netbankingloginsafe_nri.htm");
+	    driver.findElement(By.xpath(".//*[@id='wrapper']/div[2]/div[1]/ul/li[1]/a")).click();
+	    
+	    Set<String> windows = driver.getWindowHandles();
+	    Iterator<String> itr = windows.iterator();
+	    String parentWindow = itr.next();
+	    String childWindow = itr.next();
+	    
+	    driver.switchTo().window(childWindow);
+	    driver.findElement(By.xpath(".//*[@id='netbanking']")).click();
+	    Thread.sleep(5000);
+	    driver.close();
+	    driver.switchTo().window(parentWindow);
+	    driver.findElement(By.xpath(".//*[@id='element1']/div[4]/a/img")).isDisplayed();
+	}
+
+}
