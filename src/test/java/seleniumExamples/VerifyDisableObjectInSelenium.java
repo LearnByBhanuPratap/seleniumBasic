61d9c7ed7574d936d25e093d2a0a723a74776ca1
Singh, Bhanu Pratap <BhanuPratap_Singh@intuit.com>
2017-07-01 12:31:55 +0530

    add

---
+++ src/test/java/seleniumExamples/VerifyDisableObjectInSelenium.java
@@ -0,0 +1,25 @@
+package seleniumExamples;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class VerifyDisableObjectInSelenium {
+
+	WebDriver driver;
+
+	@Test
+	public void selectDropDownInSelenium() throws InterruptedException {
+		// for Mac OS
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver");
+		// For Window OS
+		// System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "/drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://www.olx.in/bangalore/cars/q-swift/");
+		String className = driver.findElement(By.xpath(".//*[@id='param_model']")).getAttribute("class");
+		Assert.assertEquals(className, "param paramSelect disabled");
+
+	}
+}
