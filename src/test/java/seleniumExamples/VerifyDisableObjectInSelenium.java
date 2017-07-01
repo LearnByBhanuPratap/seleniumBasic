ccb21db43cebc289cbfaafaa83bdca97d7fe1a89
Singh, Bhanu Pratap <BhanuPratap_Singh@intuit.com>
2017-07-01 12:41:52 +0530

    Add

--- src/test/java/seleniumExamples/VerifyDisableObjectInSelenium.java
+++ src/test/java/seleniumExamples/VerifyDisableObjectInSelenium.java
@@ -18,8 +18,17 @@ public class VerifyDisableObjectInSelenium {
 		// System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "/drivers/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("https://www.olx.in/bangalore/cars/q-swift/");
+		
 		String className = driver.findElement(By.xpath(".//*[@id='param_model']")).getAttribute("class");
 		Assert.assertEquals(className, "param paramSelect disabled");
+		
+		// Click on Brand Name
+		driver.findElement(By.xpath(".//*[@id='param_subcat']/div[2]/a/span[1]")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath(".//*[@id='param_subcat']/div[2]/ul/li[2]/a")).click();
+		
+		String className1 = driver.findElement(By.xpath(".//*[@id='param_model']")).getAttribute("class");
+		Assert.assertNotEquals(className1, "param paramSelect disabled");
 
 	}
 }
